9c3edc03acf5268ab7f841df0ace3fbe18ec9940
Nobuyuki Kondo <kondo@artprojectteam.jp>
2017-11-09 17:04:11 +0900

    development setup

---
+++ demo/zero-padding.js
@@ -0,0 +1,9 @@
+/*!
+Zero-Padding v0.0.1
+Zero padding for number at JavaScript
+Copyright (c) 2017 Nobuyuki Kondo
+License: MIT
+
+https://github.com/artprojectteam/zero-padding
+*/
+!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?module.exports=n():"function"==typeof define&&define.amd?define(n):e.ZeroPadding=n()}(this,function(){"use strict";return function(){}});
---
+++ dist/zero-padding.js
@@ -0,0 +1,19 @@
+/*!
+Zero-Padding v0.0.1
+Zero padding for number at JavaScript
+Copyright (c) 2017 Nobuyuki Kondo
+License: MIT
+
+https://github.com/artprojectteam/zero-padding
+*/
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.ZeroPadding = factory());
+}(this, (function () { 'use strict';
+
+	var index = (function () {});
+
+	return index;
+
+})));
